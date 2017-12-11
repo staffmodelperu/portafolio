1d0407f61e4f5bb3c790e3d3f759ba8952952e1c
Dracon11 <jkjokr@mail.com>
2017-12-11 00:58:32 -0500

    creacion de servicios y conexion a firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,18 +5,31 @@ import { Injectable } from '@angular/core';
 export class InformacionService {
 
   info: any = {};
-  cargada: Boolean = false;
-
+  cargadaInfo: Boolean = false;
+  cargadaAbout: Boolean= false;
+  equipo: any[] = [];
 
   constructor(public http: Http) {
+    this.cargaInfo();
+    this.cargaAbout();
+  }
 
+  public cargaInfo () {
     this.http.get('assets/data/info.pagina.json')
     .subscribe( data => {
-        console.log(data);
-        this.cargada = true;
+        // console.log(data);
+        this.cargadaInfo = true;
         this.info = data.json();
     });
+  }
 
+  public cargaAbout() {
+    this.http.get('https://portafolio-57aa1.firebaseio.com/equipo.json')
+    .subscribe( data => {
+        console.log(data);
+        this.cargadaAbout = true;
+        this.equipo = data.json();
+    });
   }
 
 
