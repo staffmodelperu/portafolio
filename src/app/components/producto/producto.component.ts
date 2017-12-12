dcc0c7975697659e2612eca168314e2759e1bfa2
Dracon11 <jkjokr@mail.com>
2017-12-12 00:05:01 -0500

    se agrego loading

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
   styles: []
 })
-export class ProductoComponent implements OnInit {
+export class ProductoComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(private route: ActivatedRoute) {
+    route.params.subscribe( parametros => {
+      console.log(parametros );
+      console.log('id', parametros['id'] );
+    });
+   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -17,9 +17,11 @@ export class ProductosService {
 
     this.http.get('https://portafolio-57aa1.firebaseio.com/productos_idx.json')
         .subscribe(res => {
-          console.log( res.json() );
-          this.cargando = false;
-          this.productos = res.json();
+          // console.log( res.json() );
+          setTimeout(() => {
+            this.cargando = false;
+            this.productos = res.json();
+          }, 1500)
         });
     }
 }
